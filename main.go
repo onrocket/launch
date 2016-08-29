9e613034aaafb9c9d6efbf802af987f1168a371a
Jon Brookes <jon@onrocket.co>
2016-08-29 19:55:11 +0100

    minor edit

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 
+	"github.com/onrocket/launch/binfiles"
 	"github.com/onrocket/launch/config"
 )
 
 func main() {
 	fmt.Printf("Move along please, nothing to see here ...\n")
 	config.LoadConfig()
+	binfiles.LoadBinFiles()
 }
