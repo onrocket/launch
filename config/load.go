682846788a2c077773614d1804e873b491303c07
Jon Brookes <jon@onrocket.co>
2016-09-03 18:28:17 +0100

    refactored fat funcs

--- config/load.go
+++ config/load.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"os/user"
@@ -10,63 +9,87 @@ import (
 	"strings"
 )
 
-func printError(err error) {
+func exitIfError(err error, reason string) {
 	if err != nil {
-		os.Stderr.WriteString(fmt.Sprintf("==> Error: %s\n", err.Error()))
+		os.Stderr.WriteString(fmt.Sprintf("==> Error: %s [%s]\n", err.Error(), reason))
+		os.Exit(1)
 	}
 }
 
-func printOutput(outs []byte) (ftype string) {
+func parseFileCmdOutput(outs []byte) (fType string) {
 	if len(outs) > 0 {
 		switch {
 		case strings.Contains(string(outs), "text"):
-			ftype = "TXT"
+			fType = "TXT"
 		case strings.Contains(string(outs), "executable"):
-			ftype = "EXT"
+			fType = "EXT"
 		}
 	}
 	return
 }
 
-// LoadConfig is currently called by main but will be moved back to this module
-func LoadConfig() {
+func launchConfigPath() string {
 
 	usr, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
-	//fmt.Println(usr.HomeDir)
-
+	exitIfError(err, "trying to get current user")
 	// TODO : create and populate onrocket launch directory if not already there
 	//        and optionaly accept an alternative directory if specified as a
 	//        command line parameter
 	searchDir := usr.HomeDir + "/.onrocket/launch"
+	return searchDir
 
+}
+
+func launchConfigFiles(searchDir string) []string {
 	fileList := []string{}
-	err = filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
+	err := filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
 		fileList = append(fileList, path)
 		return nil
 	})
-	if err != nil {
-		fmt.Printf("bumber : %s\n", err)
-		os.Exit(1)
-	}
+	exitIfError(err, fmt.Sprintf("can't read directory [%s]", searchDir))
+	return fileList
+}
 
+func findConfigs(fileList []string, searchDir string) {
 	for _, file := range fileList {
-		choppedFile := strings.Replace(file, searchDir, "", -1)
-
-		// Create an *exec.Cmd
-		cmd := exec.Command("file", "-b", file)
-
-		// Combine stdout and stderr
-		output, err := cmd.CombinedOutput()
-		printError(err)
-		ftype := printOutput(output)
-		if ftype == "TXT" {
-			fmt.Printf("got ftype [%s] back from printOutput\n", ftype)
-			fmt.Printf("\t     file[%s]\n\tetcd path[%s]\n\n", file, choppedFile)
+		fileCmd := exec.Command("file", "-b", file)
+		fileCmdOutput, err := fileCmd.CombinedOutput()
+		exitIfError(err, "running file command through exec")
+		fType := parseFileCmdOutput(fileCmdOutput)
+		if fType == "TXT" {
+			uploadFileOrConfig(file, searchDir)
 		}
-
 	}
+}
+
+func uploadFileOrConfig(file, searchDir string) {
+	choppedFile := strings.Replace(file, searchDir, "", -1)
+	ext := filepath.Ext(file)
+	fmt.Printf("\t     file[%s]\n", file)
+	fmt.Printf("\tetcd path[%s]\n", choppedFile)
+	fmt.Printf("\t      ext[%s]\n\n", ext)
+	if (ext == ".csv") || ext == ".CSV" {
+		parseUploadCSVData(file, searchDir)
+	} else {
+		uploadFileData(file, searchDir)
+	}
+}
+
+func parseUploadCSVData(file, searchDir string) {
+	fmt.Printf("about to upload CSV\n\n")
+}
+
+func uploadFileData(file, searchDir string) {
+	fmt.Printf("about to upload text file complete\n\n")
+}
+
+// LoadConfig is currently called by main but will be moved back to this module
+func LoadConfig() {
+
+	searchDir := launchConfigPath()
+
+	fileList := launchConfigFiles(searchDir)
+
+	findConfigs(fileList, searchDir)
 
 }
